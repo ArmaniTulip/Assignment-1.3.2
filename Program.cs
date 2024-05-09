b92ff787d079b607d23da2b986e9f27cf6da3e9c
Garrett <Garrett@YUP>
2024-05-09 14:42:30 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Assignment_1._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] assignmentArray = { 2, 5, 7 };
+            int n = assignmentArray.Length;
+            int temp = 0;
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                temp = assignmentArray[i];
+                assignmentArray[i] = assignmentArray[n - i - 1];
+                assignmentArray[(n - i) - 1] = temp;
+            }
+
+            foreach (int i in assignmentArray)
+
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+    }
+
+}
